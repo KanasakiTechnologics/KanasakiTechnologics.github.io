80e1a7f00a89999c7dde40355e5d69454a4b3a10
KanasakiTechnologics <kanasakitechnologics@gmail.com>
2025-11-06 14:02:53 +0800

    add on

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import HomeSection from "@/components/HomeSection";
 
 export default function page() {
   return (
     <div>
-      
+        <HomeSection/>
     </div>
   )
 }
---
+++ components/HomeSection.tsx
@@ -0,0 +1,14 @@
+
+export default function HomeSection() {
+    return (
+        <section id="home" className="snap-start h-screen">
+            <div className="flex flex-col h-full items-center justify-center text-center ">
+                <h4 className="">HTTP. CSS. JS. TailwindCSS.</h4>
+                <h1 className="">Territory set and boot-up</h1>
+                <p className="">Hello and welcome to my website</p>
+                <p className=""># As usual for Prototype and learning only ...</p>
+            </div>
+        </section>
+    );
+
+}
